2d6b5dcc9a88d0ffac24c6c0a8bdb6e23b7cf0e6
Matheus2727 <81767149+Matheus2727@users.noreply.github.com>
2021-08-27 18:12:19 -0300

    Add files via upload

---
+++ wolfram/src/main.py
@@ -0,0 +1,9 @@
+import implementar_interface
+
+
+def main():
+    implementar_interface.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ wolfram/src/problemas.py
@@ -0,0 +1,10 @@
+class Gerente:
+    def __init__(self):
+        self.run = True
+
+
+class Problema:
+    def __init__(self, nome, conteudo):
+        self.nome = nome
+        self.conteudo = conteudo
+        self.resultado = 0
